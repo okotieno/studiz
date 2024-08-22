a86c03e4c437619d0a3fdfcc3dc37338b1188ee6
Owen Kelvin <owenkelvin@Owens-MacBook-Pro.local>
2024-08-22 06:36:41 +0300

    status

--- libs/website/pages/complete-get-started/src/lib/complete-get-started.component.ts
+++ libs/website/pages/complete-get-started/src/lib/complete-get-started.component.ts
@@ -1,52 +1,67 @@
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
-import {
-  IonAccordion,
-  IonAccordionGroup, IonButton,
-  IonCol,
-  IonInput,
-  IonItem,
-  IonLabel,
-  IonRow
-} from '@ionic/angular/standalone';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
-import { IInstitutionRequestModel } from '@studiz/shared/types/frontend';
+import { Component, inject, signal } from '@angular/core';
+import { IonButton, IonCol, IonIcon, IonRow, IonSegment, IonSegmentButton } from '@ionic/angular/standalone';
+// import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+// import {
+//   IonAccordion,
+//   IonAccordionGroup, IonButton,
+//   IonCol,
+//   IonInput,
+//   IonItem,
+//   IonLabel,
+//   IonRow
+// } from '@ionic/angular/standalone';
+// import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { JsonPipe } from '@angular/common';
+// import { IInstitutionRequestModel } from '@studiz/shared/types/frontend';
 
 @Component({
   selector: 'studiz-website-complete-get-started',
   standalone: true,
   imports: [
-    IonRow,
+    IonSegment,
+    IonSegmentButton,
+    IonIcon,
     IonCol,
-    IonInput,
-    IonAccordionGroup,
-    IonAccordion,
-    IonItem,
-    IonLabel,
-    IonButton,
-    JsonPipe,
-    ReactiveFormsModule
+    IonRow,
+    IonButton
+    // IonRow,
+    // IonCol,
+    // IonInput,
+    // IonAccordionGroup,
+    // IonAccordion,
+    // IonItem,
+    // IonLabel,
+    // IonButton,
+    // JsonPipe,
+    // ReactiveFormsModule
   ],
   templateUrl: './complete-get-started.component.html',
   styleUrl: './complete-get-started.component.css',
 })
 export class CompleteGetStartedComponent {
-  progressData = inject(ActivatedRoute).snapshot.data['institutionRequest'] as IInstitutionRequestModel;
-  fb = inject(FormBuilder);
-  form = this.fb.nonNullable.group({
-    systemAdminInfo: this.fb.nonNullable.group({
-      email: [this.progressData.adminEmail, [Validators.required]],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
 
-    }),
-  })
 
-  get systemAdminInfoControl() {
-    return this.form.get('systemAdminInfo');
-  }
-  constructor() {
-    console.log('CompleteGetStartedComponent', this.progressData);
-  }
+
+
+
+
+
+  // progressData = inject(ActivatedRoute).snapshot.data['institutionRequest'] as IInstitutionRequestModel;
+  // fb = inject(FormBuilder);
+  // form = this.fb.nonNullable.group({
+  //   systemAdminInfo: this.fb.nonNullable.group({
+  //     email: [this.progressData.adminEmail, [Validators.required]],
+  //     firstName: ['', Validators.required],
+  //     lastName: ['', Validators.required],
+  //
+  //   }),
+  // })
+  //
+  // get systemAdminInfoControl() {
+  //   return this.form.get('systemAdminInfo');
+  // }
+  // constructor() {
+  //   console.log('CompleteGetStartedComponent', this.progressData);
+  // }
+  currentStep = signal<string>('systemAdminInfo');
 }

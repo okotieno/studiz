f9b0abdce178bbf85da625c2d4b70f6017308de6
Owen Kelvin <owenkelvin@Owens-MacBook-Pro.local>
2024-09-06 16:58:50 +0300

    adds

--- libs/backend/institution-request/src/lib/dto/complete-institution-request-input.dto.ts
+++ libs/backend/institution-request/src/lib/dto/complete-institution-request-input.dto.ts
@@ -3,12 +3,11 @@ import { InstitutionRequestModel } from '@studiz/backend/db';
 import { Exists } from '@studiz/backend/validators';
 
 export class CompleteInstitutionRequestInputDto {
-  // @IsString()
+  @IsString()
   @Exists(InstitutionRequestModel, 'slug', {
     message: (validationArguments) => {
       return `InstitutionRequest with id  ${validationArguments.value}" not found`
     }
-
   })
   id = 0;
 
